15bff80e0b784d563e0f661664da20409c76ffec
Michael <kamaumike2013@gmail.com>
2020-07-05 20:18:40 +0300

    Add the 'any' type

---
+++ learn_any_type.js
@@ -0,0 +1,9 @@
+var favouriteActivities; //Define an array that stores mixed type of data
+favouriteActivities = ["Sports", 5]; // e.g numbers,strings
+var myName;
+myName = "Jane Doe";
+var comments;
+comments = 4;
+console.log(favouriteActivities);
+console.log(myName);
+console.log(comments);
