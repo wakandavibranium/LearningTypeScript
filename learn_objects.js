d68e01bbaf304e1b14336787326efbe8ac65615f
Michael <kamaumike2013@gmail.com>
2020-07-05 18:22:47 +0300

    Add arrays

--- learn_objects.js
+++ learn_objects.js
@@ -1,6 +1,14 @@
 var person = {
     name: "John Doe",
-    age: 230
+    age: 230,
+    hobbies: ["Lawn tennis", "Online gaming"]
 };
 console.log(person.name);
 console.log(person.age);
+var favouriteActivities;
+favouriteActivities = ["Sports"];
+console.log(favouriteActivities);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- learn_objects.ts
+++ learn_objects.ts
@@ -1,7 +1,17 @@
 const person = {
     name: "John Doe",
-    age: 230
+    age: 230,
+    hobbies: ["Lawn tennis","Online gaming"]
 }
 
 console.log(person.name);
 console.log(person.age);
+
+let favouriteActivities: string[];
+favouriteActivities = ["Sports"];
+
+console.log(favouriteActivities);
+
+for(const hobby of person.hobbies){
+    console.log(hobby.toUpperCase());
+}
